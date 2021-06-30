9a4879b4ae30ab3770bd454463323cda77d69060
ayafrss <81585695+ayafrss@users.noreply.github.com>
2021-06-30 21:03:51 +0100

    Update TP4-EX3.c

--- TP4/TP4-EX3.c
+++ TP4/TP4-EX3.c
@@ -1,31 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct s_node
+struct node
 {
     int key;
-    struct s_node* right;
-    struct s_node* left;
+    struct node* right;
+    struct node* left;
 };
-typedef struct s_node t_node;
+typedef struct node node;
 
-//Create a new node
-t_node* create_node(int key)
+
+node* create_node(int key)
 {
-    t_node* new_node = (t_node*)malloc(sizeof(t_node));
-    new_node->key = key;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    return new_node;
+    node* new_node = (node*)malloc(sizeof(node));
+    nouveau_node->key = key;
+    nouveau_node->left = NULL;
+    nouveau_node->right = NULL;
+    return nouveau_node;
 }
-//Insert a node to a BST
-t_node* insert(t_node* binaryS_tree, int key)
+
+node* insert(node* binaryS_tree, int key)
 {
     if(binaryS_tree == NULL)
         return create_node(key);
-    t_node* new_node = create_node(key);
-    t_node* temp = binaryS_tree;
-    t_node* temp_1 = NULL;
+    node* nouveau_node = create_node(key);
+    node* temp = binaryS_tree;
+    node* temp_1 = NULL;
     while(temp != NULL)
     {
         if(temp->key > key)
@@ -47,8 +47,8 @@ t_node* insert(t_node* binaryS_tree, int key)
         temp_1->right = new_node;
     return temp_1;
 }
-//Count the number of elements in the BST
-int n_elem(t_node* binaryS_tree)
+
+int n_elem(node* binaryS_tree)
 {
     int k = 1;
     if(binaryS_tree == NULL)
@@ -56,11 +56,11 @@ int n_elem(t_node* binaryS_tree)
     k += n_elem(binaryS_tree->left) + n_elem(binaryS_tree->right);
     return k + 1;
 }
-//Convert the binary search tree to an array
-int* convertBST_array(t_node* binaryS_tree, int* array)
+
+int* convertBST_array(node* binaryS_tree, int* array)
 {
-    t_node* temp = binaryS_tree;
-    t_node* temp_1 = NULL;
+    node* temp = binaryS_tree;
+    node* temp_1 = NULL;
     array[0] = binaryS_tree->key;
     int i = 1;
     do
@@ -83,19 +83,19 @@ int* convertBST_array(t_node* binaryS_tree, int* array)
     array[i] = temp_1->key; 
     return array;    
 }
-//Create a balanced binary search tree from the sorted array below
-t_node* createBST_from_array(int* array, int s, int e) 
+
+node* createBST_from_array(int* array, int s, int e) 
 {
     if(s > e)
         return NULL;
     int m = (s+e)/2;
-    t_node* balanced_BST = create_node(array[m]);
+    node* balanced_BST = create_node(array[m]);
     balanced_BST->left = createBST_from_array(array, s, m-1);
     balanced_BST->right = createBST_from_array(array, m+1, e);
     return balanced_BST;
 }
-//Print binary search tree(Parcours préfixe):
-void print_BST(t_node* binaryS_tree)
+
+void print_BST(node* binaryS_tree)
 {
     if(binaryS_tree == NULL)
         return;
@@ -107,7 +107,7 @@ void print_BST(t_node* binaryS_tree)
 }
 int main()
 {
-    t_node* binaryS_tree = NULL;
+    node* binaryS_tree = NULL;
     binaryS_tree = insert(binaryS_tree, 1);
     binaryS_tree = insert(binaryS_tree, 2);
     binaryS_tree = insert(binaryS_tree, 3);
@@ -115,7 +115,7 @@ int main()
     printf("\n\n");
     int* array = (int*)malloc(4*sizeof(int));
     array = convertBST_array(binaryS_tree, array);
-    t_node* tree = createBST_from_array(array, 1, 4);
+    node* tree = createBST_from_array(array, 1, 4);
     print_BST(tree);
     printf("\n\n");
     return 0;
